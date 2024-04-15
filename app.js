2c56799ab293b9fd68088aca5599b1bcdfdc0172
Marion <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-04-15 14:08:19 +0200

    🔧 Add mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // app.js
 const express = require('express');
+const mongoose = require('mongoose'); 
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000; // Utilisez le port spécifié dans les variables d'environnement ou 3000 par défaut
+
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
 
 // Importez les routes définies dans apiRoutes.js
 const apiRoutes = require('./routes/apiRoutes');
