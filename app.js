2d0bfbd88f0cae8cef301a3674b64d41ee06d536
matz57 <matz.jere.pro@gmail.com>
2024-04-19 16:02:14 +0200

    :wrench: Config doker et mongodb

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // app.js
+require('dotenv').config({ path: './.env' });
 const express = require('express');
 const mongoose = require('mongoose'); 
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000; // Utilisez le port spécifié dans les variables d'environnement ou 3000 par défaut
 
-require('dotenv').config();
-
-const mongoString = process.env.DATABASE_URL
+const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
+database
+	.once('open', () => {
+		console.log('mongoDB database connection established');
+	})
+	.on('error', err => {
+		console.log('Error: ', err);
+	});
 
 // Importez les routes définies dans apiRoutes.js
 const apiRoutes = require('./routes/apiRoutes');
