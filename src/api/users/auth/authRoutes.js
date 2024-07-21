c7ba6e7cd5247bf7bcc1da891fcec1f57f166a43
Marion Rollando <rollando.marion@gmail.com>
2024-07-21 16:49:16 +0200

    ✨ Add forgot-password route in authRoutes

--- src/api/users/auth/authRoutes.js
+++ src/api/users/auth/authRoutes.js
@@ -6,6 +6,7 @@ const { authMiddleware } = require('./authMiddleware');
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/validate-token', authMiddleware, authController.validateToken);
+router.post('/forgot-password', authController.forgotPassword);
 router.get('/is-admin', authMiddleware, authController.isAdmin);
 router.get('/is-superadmin', authMiddleware, authController.isSuperAdmin);
 
